162211b6d423eddd16b111c3a2d42ac3c90aecf9
david nelband <dnelband@gmail.com>
2021-07-31 21:49:27 +0200

    add image upload functionality to textEditor component

--- src/app/partials/TextEditor.js
+++ src/app/partials/TextEditor.js
@@ -1,19 +1,30 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import '../style/text-editor.css';
 import axios from 'axios';
+import $ from 'jquery';
 
 function TextEditor(props) {
-  const [showHtml, setShowHtml] = useState(false);
-  let initText = props.val ? props.val : '';
-  const [convertedText, setConvertedText] = useState(initText);
 
+  const [showHtml, setShowHtml] = useState(false);
+  const [convertedText, setConvertedText] = useState(props.val ? props.val : '');
   const [file, setFile] = useState();
   const [fileData, setFileData] = useState();
 
-  const reader = new FileReader();
+  // updates parent component on value change
 
+  useEffect(() => {
+    props.onTextEditorUpdate(convertedText);
+  }, [convertedText]);
+
+  // reference to the editor object
+
+  let quillObj = useRef();
+  console.log(quillObj);
+  /* Image Upload Handlers */
+
+  const reader = new FileReader();
   reader.addEventListener(
     'load',
     function () {
@@ -23,41 +34,34 @@ function TextEditor(props) {
     false
   );
 
-  let quillObj = useRef();
-
-  useEffect(() => {
-    props.onTextEditorUpdate(convertedText);
-  }, [convertedText]);
-
   async function imageHandler() {
     const input = document.createElement('input');
-
     input.setAttribute('type', 'file');
     input.setAttribute('accept', 'image/*');
     input.click();
-
     input.onchange = async () => {
       var file: any = input.files[0];
       reader.readAsDataURL(file);
       setFile(file);
-      // const res = await uploadFiles(file, fileName, quillObj);
+      const res = await uploadFile(file, file.name, quillObj.current);
     };
   }
 
-  const uploadFile = () => {
+  const uploadFile = (file, fileName, quillObj) => {
     const formData = new FormData();
     formData.append('file', file); // appending file
     axios
       .post('/upload', formData)
       .then(res => {
         const range = quillObj.getEditorSelection();
-        quillObj.getEditor().insertEmbed(range.index, 'image', res.data.path);
-        // props.onFinishUpload(res.data);
+        quillObj.getEditor().insertEmbed(range.index, 'image', '/' + res.data.path);
       })
       .catch(err => console.log(err));
   };
 
-  const modules = {
+  /* Editor Config */
+
+  const modules = useMemo(() => ({
     toolbar: {
       container: [
         [{ header: [1, 2, false] }],
@@ -73,19 +77,22 @@ function TextEditor(props) {
         ['link', 'image'],
         ['clean'],
       ],
+      handlers: {
+        image: imageHandler
+      }
     },
     clipboard: {
       matchVisual: false,
     },
-  };
+  }),[]);
+
+  /* Editor Display */
 
   let textareDisplay;
   if (showHtml === false) {
     textareDisplay = (
       <ReactQuill
-        ref={el => {
-          quillObj = el;
-        }}
+        ref={quillObj}
         theme="snow"
         value={convertedText}
         onChange={setConvertedText}
